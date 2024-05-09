ad6dad31d2075b1a385e4c435ca45a6f5d73bea5
Grant L. <56404949+GrantjyLi@users.noreply.github.com>
2024-05-08 22:01:47 -0400

    Wifi test done
    have to use a 2.4ghz network, did not know that iphone 13 hotspot was default 5ghz

---
+++ sketch_may8a/wifiTest/wifiTest.ino
@@ -0,0 +1,32 @@
+#include <ESP8266WiFi.h>        // Include the Wi-Fi library
+
+const char* ssid = "GPhone";
+const char* password = "hotspot4Grant";
+
+void setup() {
+   // Start serial
+  Serial.begin(115200);
+  delay(10);
+
+  // Connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  
+}
