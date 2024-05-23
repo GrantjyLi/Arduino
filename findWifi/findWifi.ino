684a1f7bf80d0610216ad7b25860bf9102a50829
Grant L. <56404949+GrantjyLi@users.noreply.github.com>
2024-05-23 18:15:48 -0400

    adapted findWifi.ino
    to continuously find new networks

--- findWifi/findWifi.ino
+++ findWifi/findWifi.ino
@@ -1,5 +1,10 @@
 #include <ESP8266WiFi.h>
 
+#define NUMSSID 30
+
+uint8_t numNetworks = 0;
+String knownSSIDs[NUMSSID];
+
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
@@ -8,19 +13,49 @@ void setup() {
   delay(1000); // Wait for the module to disconnect
 
   Serial.println("Scanning for available networks:");
-  int numNetworks = WiFi.scanNetworks();
+  numNetworks = WiFi.scanNetworks();
   
   if (numNetworks == 0) {
     Serial.println("No networks found.");
   } else {
     Serial.print(numNetworks);
     Serial.println(" networks found:");
-    for (int i = 0; i < numNetworks; ++i) {
+    for (int i = 0; i < numNetworks; i++) {
+      knownSSIDs[i] = WiFi.SSID(i);
+      Serial.println("--------------------------");
+      Serial.print("SSID: ");
       Serial.println(WiFi.SSID(i));
+      Serial.print("Strength: ");
+      Serial.println(WiFi.RSSI(i));
     }
   }
 }
 
-void loop() {
-  
+void loop(){
+  delay(5000);
+
+  uint8_t newNumNetworks = WiFi.scanNetworks();
+
+  for (uint8_t i = 0; i < newNumNetworks; i++){
+    String newSSID = WiFi.SSID(i);
+    bool newNetwork = true;
+
+    for (uint8_t k = 0; k < numNetworks; k++){
+      if(newSSID == knownSSIDs[k]){
+        newNetwork = false;
+        break;
+      }
+    }
+
+    if(newNetwork){
+      Serial.println("--------------------------");
+        Serial.print("SSID: ");
+        Serial.println(newSSID);
+        Serial.print("Strength: ");
+        Serial.println(WiFi.RSSI(i));
+
+        knownSSIDs[numNetworks] = newSSID;
+        numNetworks++;
+    }
+  }
 }
