50c77f6373bcf8e596611f712f37703b6f003b5b
Grant L. <56404949+GrantjyLi@users.noreply.github.com>
2024-05-24 13:13:57 -0400

    trying to get user options and set up continuous mimiced AP

--- Mimic_networks/Mimic_networks.ino
+++ Mimic_networks/Mimic_networks.ino
@@ -10,102 +10,119 @@ IPAddress gateway(192,168,4,9);
 IPAddress subnet(255,255,255,0);
 
 void setup() {
-    Serial.begin(115200);
-    Serial.flush();
-    printInstruction();
 
     WiFi.mode(WIFI_STA);
     WiFi.disconnect(); // Disconnect from any previously connected network
 
     delay(1000); // Wait for the module to disconnect
 
-    Serial.println("\nScanning for available networks:");
-    numNetworks = WiFi.scanNetworks();
-
-    if (numNetworks == 0) {
-    Serial.println("No networks found.");
-    } else {
-    Serial.println("Networks found:");
-    for (int i = 0; i < numNetworks; i++) {
-        knownSSIDs[i] = WiFi.SSID(i);
-        Serial.println("--------------------------");
-        Serial.printf("Network #: %d\n", i);
-        Serial.print("SSID: ");
-        Serial.println(WiFi.SSID(i));
-        Serial.print("Strength: ");
-        Serial.println(WiFi.RSSI(i));
-    }
-    }
-
+    Serial.begin(115200);
+    Serial.flush();
+    printInstruction();
 
 }
 
 void printInstruction(){
-    Serial.println("Enter 1 to refresh for networks.");
+    Serial.println("\nEnter 1 to refresh for networks.");
     Serial.println("Enter 2 to mimic a found network SSID.");
     Serial.println("Enter 3 to create a unique network SSID.\n");
 }
 
 void loop(){
     delay(5000);
-    char input = getInput();
+    while (!Serial.available()) {
+        // Wait for user input
+    }
+
+    char input = Serial.read();
     switch (input){
-        case 1:
+        case '1':
             findNewNetworks();
             break;
-        case 2:
+        case '2':
             MimicNetwork();
             break;
-        case 3:
-            createUniqueAP();
+        case '3':
+            createUniqueAP(getSSIDInput());
             break;
         default:
+            Serial.println("Enter a Valid Answer: ");
+            printInstruction();
             break;
     }
-
-}
-
-char getInput(){
-    if (Serial.available() > 0) {
-        char input = Serial.read();
-        return input;
-    }
-    return 0;
 }
 
 void findNewNetworks(){
+    Serial.println("\nScanning for available networks:");
+
     uint8_t newNumNetworks = WiFi.scanNetworks();
 
     for (uint8_t i = 0; i < newNumNetworks; i++){
-    String newSSID = WiFi.SSID(i);
-    bool newNetwork = true;
+        String newSSID = WiFi.SSID(i);
+        bool newNetwork = true;
 
-    for (uint8_t k = 0; k < numNetworks; k++){
-        if(newSSID == knownSSIDs[k]){
-        newNetwork = false;
-        break;
+        for (uint8_t k = 0; k < numNetworks; k++){
+            if(newSSID == knownSSIDs[k]){
+            newNetwork = false;
+            break;
+            }
         }
-    }
 
-    if(newNetwork){
-        Serial.println("--------------------------");
-        Serial.printf("Network #: %d\n", numNetworks);
-        Serial.print("SSID: ");
-        Serial.println(newSSID);
-        Serial.print("Strength: ");
-        Serial.println(WiFi.RSSI(i));
-
-        knownSSIDs[numNetworks] = newSSID;
-        numNetworks++;
-    }
+        if(newNetwork){
+            printNetwork(WiFi.SSID(i), WiFi.RSSI(i), numNetworks);
+            knownSSIDs[numNetworks] = newSSID;
+            numNetworks++;
+        }
     }
 }
 
 void MimicNetwork(){
+    Serial.println("Which network # to mimic: ");
+    printAllNetwork();
 
+    char input = Serial.read();
+
+    if(input >= 0 && input < numNetworks){
+        createUniqueAP(knownSSIDs[input]);
+    }else{
+        Serial.println("Invalid network number.");
+    }
+    
+    
 }
 
 void createUniqueAP(String SSID){
     WiFi.softAPConfig(local_IP, gateway, subnet);
     WiFi.softAP(SSID);
+}
+
+void printAllNetwork(){
+    for (uint8_t i = 0; i < numNetworks; i++){
+        printNetwork(WiFi.SSID(i), WiFi.RSSI(i), i);
+    }
+    
+}
+
+void printNetwork(String SSID, uint8_t strength, uint8_t numNetwork){
+    Serial.println("--------------------------");
+    Serial.printf("Network #: %d\n", numNetwork);
+    Serial.print("SSID: ");
+    Serial.println(SSID);
+    Serial.print("Strength: ");
+    Serial.println(strength);
+}
+
+String getSSIDInput(){
+    String inputString = "";
+    while (Serial.available()) {
+        char inChar = (char)Serial.read();
+
+        inputString += inChar;
+
+        if (inChar == '\n') {
+            inputString.trim();
+            break;
+        }
+    }
+    return inputString;
 }
