13f3fad4d0014a2dce8e8eaeb56ddfeb404534fa
Grant L. <56404949+GrantjyLi@users.noreply.github.com>
2024-06-20 16:47:40 -0400

    still don\ens't do anything anything

--- Beacon_spam/Beacon_spam.ino
+++ Beacon_spam/Beacon_spam.ino
@@ -3,7 +3,12 @@
 
 using namespace defs;
 
-bool wpa2 = false;
+extern "C" {
+    #include "user_interface.h"
+    int wifi_send_pkt_freedom(uint8 *buf, int len, bool sys_seq);
+}
+
+const bool wpa2 = false;
 char emptySSID[32];
 uint8_t macAdd[6];
 uint8_t wifiChannel;
@@ -13,13 +18,14 @@ char ssid[] = "cheese\n";
 
 void setup(){
     packetSize = sizeof(beaconPacket);
-    initSSID(emptySSID);
+    initSSID();
 
     Serial.begin(115200);
     Serial.println();
     
     WiFi.mode(WIFI_OFF);
     wifi_set_opmode(STATION_MODE);
+    wifi_promiscuous_enable(0);
 
     if (wpa2) {
         beaconPacket[34] = 0x31;
@@ -32,7 +38,7 @@ void setup(){
 void loop(){
 
     for(int i=0; i<NUM_BEACONS; i++){
-        randomMac(macAdd);
+        randomMac();
 
         //copy fake mac address into the packet
         memcpy(&beaconPacket[10], macAdd, 6);
@@ -40,7 +46,7 @@ void loop(){
 
         //reset and write ssid into the packet
         memcpy(&beaconPacket[38], emptySSID, 32);
-        memcpy_P(&beaconPacket[38], ssid, sizeof(ssid));
+        memcpy_P(&beaconPacket[38], ssid, sizeof(ssid) -1);
 
         //switch wifi channels
         for(int i=0; i< sizeof(channels); i++){
@@ -48,22 +54,27 @@ void loop(){
             beaconPacket[82] = wifiChannel;
             wifi_set_channel(wifiChannel);
 
-            wifi_send_pkt_freedom(beaconPacket, packetSize, 0);
+            if(wifi_send_pkt_freedom(beaconPacket, packetSize, 0) != 0 ){
+                Serial.println("Packet Failed to send.");
+            }else{
+                Serial.println("Packet Sent Successfully.");
+            }
+            
             delay(1);
         }
     }
 
-    
+    delay(10000);
 }
 
-void randomMac(uint8_t * macAdd){
+void randomMac(){
     for(int i=0; i< 6; i++){
         macAdd[i] = random(256);
     }
 }
 
-void initSSID(char* SSID){
+void initSSID(){
     for(int i=0; i< 32; i++){
-        SSID[i] = ' ';
+        emptySSID[i] = ' ';
     }
 }
